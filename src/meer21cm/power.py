3d1f440d8d4e37a99da07f7cc5256331c09a4bbb
Zhaoting Chen <ztchenustc@gmail.com>
2025-09-27 02:00:04 +0100

    fix: rewind sigmaz

--- src/meer21cm/power.py
+++ src/meer21cm/power.py
@@ -823,12 +823,8 @@ class ModelPowerSpectrum(CosmologyCalculator):
             * self.fog_term(self.deltav_to_deltar(sigma_v_2), mumode=mumode)
             # fog is exp(-k^2 sigma^2 / 2), whereas redshift error
             # is exp(-k^2 deltar^2)
-            * self.fog_gaussian(
-                self.deltaz_to_deltar(sigma_z_1) * np.sqrt(2), mumode=mumode
-            )
-            * self.fog_gaussian(
-                self.deltaz_to_deltar(sigma_z_2) * np.sqrt(2), mumode=mumode
-            )
+            * self.fog_gaussian(self.deltaz_to_deltar(sigma_z_1), mumode=mumode)
+            * self.fog_gaussian(self.deltaz_to_deltar(sigma_z_2), mumode=mumode)
         )
         return power_in_redshift_space
 
