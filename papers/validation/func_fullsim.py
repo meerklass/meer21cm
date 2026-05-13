898c15c8549890fecb8bc9fd8493dc52eca642be
Zhaoting Chen <ztchenustc@gmail.com>
2026-05-14 00:08:48 +0200

    feat: include batch number in tf calculation

--- papers/validation/func_fullsim.py
+++ papers/validation/func_fullsim.py
@@ -9,7 +9,11 @@ from meer21cm.power import get_shot_noise_galaxy
 from meer21cm.grid import shot_noise_correction_from_gridding
 from astropy.cosmology import Planck18
 from meer21cm.util import pca_clean
-
+from meer21cm.transfer import (
+    TransferFunction,
+    run_tf_calculation_auto,
+    run_tf_calculation_cross,
+)
 # generate the foreground template once
 # from meer21cm.fg import ForegroundSimulation
 # fgsim = ForegroundSimulation(
@@ -115,6 +119,37 @@ def get_3d_power(seed):
     mock.apply_taper_to_field(2, axis=[0, 1, 2])
     phiclean3d = mock.auto_power_3d_1
     pxclean3d = mock.cross_power_3d
+
+    # transfer function
+    mock.k1dbins = k1dbins
+
+    tf = TransferFunction(
+        mock,
+        N_fg=N_fg,
+        highres_sim=None,
+        # generate mock data on a high-resolution grid, then to average it to sky map for injection
+        upres_transverse=2,
+        upres_radial=2,
+        uncleaned_data=tot_map,  # inject into the map data to reperform PCA
+        # R_mat=R_mat,
+        num_process=1,  # number of available cpus to run parallel calculation
+        pca_map_weights=mock.W_HI.astype("float"),
+        unmask_during_mock=True,
+        discrete_source_dndz=[
+            z_cen,
+            z_count / dV_arr,
+        ],
+    )
+    # arglist = tf.get_arg_list_for_parallel_auto(
+    arglist = tf.get_arg_list_for_parallel_cross(
+        np.array([mock.seed + 10000]),  # make sure it is a different seed
+        return_power_3d=True,
+        return_power_1d=False,
+    )
+    # result = run_tf_calculation_auto(*arglist[0])
+    result = run_tf_calculation_cross(*arglist[0])
+    power_tf_before = result[1]
+    power_tf_after = result[2]
     np.savez(
         f"/scratch3/users/ztchen/validation/full_sim_seed_{seed}.npz",
         phi3d=pdata3d,
@@ -126,6 +161,8 @@ def get_3d_power(seed):
         phiclean3d=phiclean3d,
         pxclean3d=pxclean3d,
         R_mat=R_mat,
+        power_tf_before=power_tf_before,
+        power_tf_after=power_tf_after,
     )
     return 1
 
